faa1c5063bee836cd4de758f76dfb31cdb9ce748
Daniel de Sao Jose <danieldsj@gmail.com>
2019-10-20 07:31:59 -0400

    Made it run regardless of whether WEBHOOK_URL is present.

--- run.sh
+++ run.sh
@@ -1,8 +1,15 @@
-# Ensure we specify a webhook url.
-[[ -z "${WEBHOOK_URL}" ]] && { echo "Environment variable WEBHOOK_URL not set"; exit 1; }
-
 # Grab a random challenge.
 EXERCISE="$(shuf -n 1 exercises.txt)"
 
-# Do the post.
-curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"${EXERCISE}\"}" $WEBHOOK_URL
+# Check if WEBHOOK_URL is present.
+if [ -z "${WEBHOOK_URL}"]
+then
+	# If no environment varibale, send error to stderr.
+	echo "No WEBHOOK_URL detected, did not perform POST." >> /dev/stderr
+else
+	# Otherwise, do POST.
+	curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"${EXERCISE}\"}" $WEBHOOK_URL
+fi
+
+# Output to exercise to stdout.
+echo "${EXERCISE}"
